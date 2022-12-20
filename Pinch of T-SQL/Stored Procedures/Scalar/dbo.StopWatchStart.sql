535b21e7265e066fada015a67deef9c188d76ccb
Karcan, Ozbal <karcanozbal@hotmail.com.tr>
2022-12-20 22:15:10 +0300

    Add project files.

---
+++ Pinch of T-SQL/Stored Procedures/Scalar/dbo.StopWatchStart.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:				<Ozbal, Karcan>
+-- Create date:			<2022-12-20 yyyyMMdd>
+-- Description:			<Stop Watch Start>
+-- Execution script:	EXEC StopWatchStop @pStopWatch = @vStopWatch, @pElapsedMS = @vElapsedMS OUTPUT, @pElapsedTime = @vElapsedTime OUTPUT
+-- =============================================
+CREATE PROCEDURE [dbo].[StopWatchStart]
+AS
+BEGIN
+	DECLARE @vStart		DATETIME2(7)	= SYSUTCDATETIME(),
+			@vResult	INT				= DATEDIFF(SECOND, {d '1970-01-01'}, GETDATE());
+
+	DECLARE @vKey		NVARCHAR(13)	= CONCAT(N'SW:', @vResult)
+
+	EXEC sp_set_session_context @vKey, @vStart
+
+	RETURN @vResult
+END
---
+++ Pinch of T-SQL/Stored Procedures/Scalar/dbo.StopWatchStop.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:				<Ozbal, Karcan>
+-- Create date:			<2022-12-20 yyyyMMdd>
+-- Description:			<Stop Watch Stop>
+-- Execution script:	DECLARE @vStopWatch INT, @vElapsedMS INT, @vElapsedTime TIME(7);
+--						EXEC @vStopWatch = StopWatchStart
+-- =============================================
+CREATE PROCEDURE [dbo].[StopWatchStop]
+	@pStopWatch		INT,
+	@pElapsedMS		INT		= NULL	OUTPUT,
+	@pElapsedTime	TIME(7)	= NULL	OUTPUT
+AS
+BEGIN
+	DECLARE @vEnd	DATETIME2(7) = SYSUTCDATETIME(),
+			@vStart DATETIME2(7) = CAST(SESSION_CONTEXT(CONCAT(N'SW:', @pStopWatch)) AS DATETIME)
+
+	SET @pElapsedMS		= DATEDIFF(MILLISECOND, @vStart, @vEnd)
+	SET @pElapsedTime	= DATEADD(MILLISECOND, @pElapsedMS, 0)
+
+	RETURN;
+END
